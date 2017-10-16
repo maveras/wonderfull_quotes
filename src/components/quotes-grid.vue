8980864bd39268fc1e22b99f9d09b79cf1065e58
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2017-10-16 18:35:17 -0300

    max quotes, y remove info added

--- src/components/quotes-grid.vue
+++ src/components/quotes-grid.vue
@@ -1,5 +1,11 @@
 <template>
 <div class="row">
+  <div class="alert alert-danger" v-if="quotes.length == 10">
+    <strong>Info!</strong> You can't add more than 10 quotes
+  </div>
+  <div class="alert alert-info" v-if="quotes.length > 0">
+    <strong>Info!</strong> Click on a quote to delete it!
+  </div>
   <div class="col-md-12">
     <quote :quotes="quotes" @click.native="removeQuote(index)" v-for="(quote,index) in quotes" :keys="quote"> {{ quote }} </quote>
   </div>
